96ec789870e36241cf458c3442fd4de070847694
Stepland <10530295-Buggyroom@users.noreply.gitlab.com>
2022-04-03 17:03:08 +0200

    more nowide testst

--- tests/nowide.cpp
+++ tests/nowide.cpp
@@ -13,7 +13,12 @@ int main() {
     if (_filepath == nullptr) {
         nowide::cerr << "No file chosen" << std::endl;
     }
+    nowide::cout << "_filepath received, seen through nowide::cout : " << _filepath << std::endl;
+    std::cout << "_filepath received, seen through std::cout : " << _filepath << std::endl;
     auto filepath = std::filesystem::path{_filepath};
+    nowide::cout << "_filepath passed to std::filesystem::path, seen through nowide::cout : " << filepath << std::endl;
+    std::cout << "_filepath passed to std::filesystem::path, seen through std::cout : " << filepath << std::endl;
+
     nowide::ifstream f(filepath.string().c_str()); // argv[1] - is UTF-8
     if(not f) {
         nowide::cerr << "Can't open " << filepath << std::endl;
