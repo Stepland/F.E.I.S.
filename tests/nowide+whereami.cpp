9b0cabaed9fd32884bb46c3547e5efb5cfffa015
Stepland <10530295-Buggyroom@users.noreply.gitlab.com>
2022-12-28 10:15:22 +0100

    Updates to tests programs

--- tests/nowide+whereami.cpp
+++ tests/nowide+whereami.cpp
@@ -27,15 +27,22 @@ void check_file(T& file) {
 int main() {
     std::string executable_folder = whereami::executable_dir();
     nowide::cout << "whereami::executable_dir() = " << executable_folder << std::endl;
+
+    auto direct_u8path = std::filesystem::u8path(whereami::executable_dir());
+    nowide::cout << "u8path{whereami::executable_dir()} = " << direct_u8path << std::endl;
 	
     auto u8string = std::u8string(executable_folder.begin(), executable_folder.end());
-    nowide::cout << "std::u8string{_)} = " << reinterpret_cast<const char*>(u8string.c_str()) << std::endl;
+    nowide::cout << "std::u8string{_} = " << reinterpret_cast<const char*>(u8string.c_str()) << std::endl;
 
     std::filesystem::path u8path{u8string};
     nowide::cout << "std::filesystem::path{_} = " << u8path << std::endl;
 
-	nowide::cout << "nowide::ifstream{_} : " << std::endl;
-	nowide::ifstream file_from_path(u8path / "test_file.txt");
+	nowide::cout << "nowide::ifstream{_.string()} : " << std::endl;
+	nowide::ifstream file_from_path((u8path / "test_file.txt").string());
 	check_file(file_from_path);
+
+    nowide::cout << "nowide::ifstream{direct_u8path.string()} : " << std::endl;
+	nowide::ifstream file_from_path2((direct_u8path / "test_file.txt").string());
+	check_file(file_from_path2);
     return 0;
 }
